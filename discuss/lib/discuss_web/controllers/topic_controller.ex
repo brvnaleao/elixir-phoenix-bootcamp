4b72325e78dd7a482edfe0594cdb4701d27004a7
Bruna Lima Leao <lleaobruna@gmail.com>
2021-03-14 19:51:08 -0300

    added update/edit routes -wip

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -14,7 +14,7 @@ defmodule DiscussWeb.TopicController do
     changeset = Topic.changeset(topic)
 
     case Discuss.Repo.insert(changeset) do
-      {:ok, _post} ->
+      {:ok, _topic} ->
         conn
         |> put_flash(:info, "Topic Created")
         |> redirect(to: Routes.topic_path(conn, :index))
@@ -30,5 +30,29 @@ defmodule DiscussWeb.TopicController do
     render conn, "index.html", topics: topics
   end
 
+  def edit(conn, %{"id" => topic_id}) do
+
+    topic = Discuss.Repo.get(Topic, topic_id) # returns a struct
+    changeset = Map.from_struct(topic)
+    |> Topic.changeset() #expects a map
+
+    render conn, "edit.html", changeset: changeset, topic: topic
+  end
+
+  def update(conn, %{"id" => topic_id, "topic" => title}) do
+
+    topic = Discuss.Repo.get!(Topic, topic_id)
+    changeset = Ecto.Changeset.change topic, %{title: title["title"]}
+
+    case Discuss.Repo.update changeset do
+      {:ok, _struct} ->
+        conn
+        |> put_flash(:info, "Topic Updated")
+        |> redirect(to: Routes.topic_path(conn, :index))
+      {:error, changeset} ->
+          render conn, "edit.html", changeset: changeset
+    end
+
+  end
 
 end
